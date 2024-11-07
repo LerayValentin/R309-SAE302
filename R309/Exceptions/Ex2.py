ec17a8737b83962bb86fbaec12a66566947f0422
LerayValentin <valentin.leray@uha.fr>
2024-11-07 22:00:32 +0400

    Changements dans exceptions

--- R309/Exceptions/Ex2.py
+++ R309/Exceptions/Ex2.py
@@ -1,11 +1,21 @@
+import os
+print("Repertoire courant :", os.getcwd())
+
+nom_fichier = "../R309-SAE302/R309/Exceptions/fichier.txt"
+
 try:
-    fichier = open("fichier.txt", "r")
-except FileNotFoundError: #si erreur Fichier non trouvé
-    print("Fichier non trouve")
-else:
-    print(fichier.read())
+    with open(nom_fichier, 'r') as fichier:
+        for ligne in fichier:
+            ligne = ligne.rstrip("\n\r")
+            print(ligne, end=' ')
+
+except FileNotFoundError:
+    print("Fichier introuvable.")
+except IOError:
+    print("Erreur d'entrée/sortie")
+except FileExistsError:
+    print("Le fichier existe déjà")
+except PermissionError:
+    print("Pas les permissions nécessaires")
 finally:
-    try:
-        fichier.close()
-    except NameError:
-        print("Le fichier n'a pas ete ouvert")
+    print("Programme execute.")
